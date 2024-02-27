464d6fbb6a0b1379bc53f171b5e5f0c070c328be
parica <parisanaderi456@gmail.com>
2024-02-27 12:55:28 +0330

    migration files

---
+++ database/migrations/2024_02_27_084028_create_articles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+
+            $table->id();
+            $table->string('title');
+            $table->text('excerpt');
+            $table->longText('content');
+            $table->string('slug');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('user_create')->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
